72bb3751d311f88f4d532db7f9478f1a88b40aa3
Oleksandr Deineka <deyneka.alexandr@gmail.com>
2020-07-31 22:02:30 +0300

    add unit test skeletons

---
+++ src/test/java/com/item/service/controller/ItemControllerTest.java
@@ -0,0 +1,17 @@
+package com.item.service.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getTheLowestPriceItem() {
+    }
+}
---
+++ src/test/java/com/item/service/service/ItemServiceTest.java
@@ -0,0 +1,21 @@
+package com.item.service.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByPrice() {
+    }
+}
