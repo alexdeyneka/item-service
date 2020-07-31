7794936fb372ef87fcd653f42670af0905c1e258
Oleksandr Deineka <deyneka.alexandr@gmail.com>
2020-07-31 22:37:41 +0300

    covers ItemService with unit tests, add TestDataGenerator, minor fixes

--- src/main/java/com/item/service/dto/ItemDTO.java
+++ src/main/java/com/item/service/dto/ItemDTO.java
@@ -1,10 +1,16 @@
 package com.item.service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class ItemDTO {
-    private long id;
+    private Long id;
     private String name;
     private double price;
     private int quantity;
--- src/test/java/com/item/service/service/ItemServiceTest.java
+++ src/test/java/com/item/service/service/ItemServiceTest.java
@@ -1,21 +1,50 @@
 package com.item.service.service;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.item.service.dto.ItemDTO;
+import com.item.service.dto.ItemMapper;
+import com.item.service.entity.Item;
+import com.item.service.repository.ItemRepo;
+import com.item.service.utils.TestDataGenerator;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
 class ItemServiceTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Mock
+    private ItemRepo itemRepo;
+
+    @InjectMocks
+    private ItemService itemService;
+
+    private final List<Item> itemList = new TestDataGenerator().generateItemList();
 
     @Test
     void findAll() {
+        when(itemRepo.findAll()).thenReturn(itemList);
+        List<ItemDTO> testList = itemService.findAll();
+        assertEquals(testList.size(), itemList.size());
+        verify(itemRepo, times(1)).findAll();
+        verify(itemRepo, only()).findAll();
     }
 
     @Test
     void findByPrice() {
+        when(itemRepo.findAll()).thenReturn(itemList);
+        Optional<ItemDTO> optionalItem = itemService.findByPrice(itemList.get(0).getName(), itemList.get(0).getQuantity());
+        assertEquals(itemList.get(0), ItemMapper.INSTANCE.itemDTOToItem(optionalItem.get()));
+        verify(itemRepo, times(1)).findAll();
+        verify(itemRepo, only()).findAll();
     }
 }
